4e726eeb02bca30f4cedc32c2b973126bc422e7f
Jehicoob Lopez <jehicoob.dev@gmail.com>
2025-01-05 00:25:21 -0500

    Add portfolio to github

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [tailwind()],
+    i18n: {
+        defaultLocale: "es",
+        locales: ["es", "en"],
+        routing: {
+            prefixDefaultLocale: true,
+            redirectToDefaultLocale: true,
+        },
+    },
+});
---
+++ src/i18n/ui.ts
@@ -0,0 +1,180 @@
+export const languages = {
+    en: "EN",
+    es: "ES",
+};
+
+export const defaultLang = "es";
+
+export const ui = {
+    en: {
+        //Header
+        "header.experience": "Experience",
+        "header.projects": "Projects",
+        "header.technologyStack": "Technology Stack",
+        "header.education": "Education",
+        "header.certificates": "Certificates",
+        "header.aboutMe": "About Me",
+        "header.contact": "Contact",
+        // HomePage
+        "home-page.title": "Portfolio | Jehicoob",
+        "home-page.description": "Welcome to my portfolio",
+        "home-page.experience": "Experience",
+        "home-page.projects": "Projects",
+        "home-page.technologyStack": "Technology Stack",
+        "home-page.education": "Education",
+        "home-page.certificates": "Certificates",
+        "home-page.aboutMe": "About Me",
+        // Hero
+        "hero.title": "Jehicoob López",
+        "hero.description.profession": "Full-Stack Developer",
+        "hero.description.degree": "Electronics Technologist",
+        "hero.description.part-1": "More than 1 year of experience.",
+        "hero.description.part-2":
+            "from Cali, Colombia 🇨🇴, passionate about transforming ideas into effective and functional digital solutions.",
+        // Experience
+        "experience.company-1-name": "Freelancer",
+        "experience.company-1-position": "Full-Stack Developer",
+        "experience.company-1-start": "may. 2022",
+        "experience.company-1-end": "Present",
+        "experience.company-1-description":
+            "Responsible for ongoing maintenance and technical support, ensuring the stability and functionality of web applications. Design and development of a chat system integrated into their websites, with more than 150 active users per day.",
+        "experience.company-2-name": "Avidesa Mac Pollo S.A.",
+        "experience.company-2-position": "Systems Technician (Internship)",
+        "experience.company-2-start": "oct. 2021",
+        "experience.company-2-end": "apr. 2022",
+        "experience.company-2-description":
+            "Provided technical support to internal users, solving incidents related to hardware, software and networks. Performed basic tasks of system maintenance, equipment configuration and IT process documentation, contributing to the department's operational efficiency.",
+        // Projects
+        "projects.chat-title": "Business Chat",
+        "projects.chat-description":
+            "Business chat with sending files, audios and images. More than 150 active users per day. Built from scratch with Vue.js, Firebase, and Tailwind CSS.",
+        "projects.system-title": "Business Management System",
+        "projects.system-description":
+            "Comprehensive platform for talent management and development in the professional field. Built with Laravel, jQuery, JavaScript and CSS.",
+        // Technology Stack
+        "technology-stack.languages": "Lenguajes",
+        "technology-stack.fronted": "Frontend",
+        "technology-stack.backend": "Backend",
+        "technology-stack.database": "Databases",
+        "technology-stack.tools": "Tools",
+        // Education
+        "education.education-1-name": "Universidad del Valle",
+        "education.education-1-degree": "Electronics Technologist",
+        "education.education-1-start": "jun. 2017",
+        "education.education-1-end": "jul. 2022",
+        "education.education-1-description":
+            "Training in electronic systems, digital and analog circuits, design and maintenance of electronic devices, applying knowledge in industrial and technological contexts.",
+        "education.education-2-name":
+            "SENA - Centro Nacional de Asistencia Técnica a la industria (ASTIN)",
+        "education.education-2-degree": "Systems Technician",
+        "education.education-2-start": "ene. 2021",
+        "education.education-2-end": "apr. 2022",
+        "education.education-2-description":
+            "Focus on technical support, hardware and software maintenance, installation of operating systems and basic networks, providing efficient solutions to computer-related issues.",
+        // About Me
+        "about-me.underlined-1": "self-taught student",
+        "about-me.underlined-2":
+            "developing new features and improving existing ones on the web platform",
+        "about-me.paragraph-1-part-1":
+            "Hello! My name is Jehicoob, and my journey in programming started with university projects focused on Arduino. Since I finished university, I have been a",
+        "about-me.paragraph-1-part-2":
+            "exploring the world of technology and software development",
+        "about-me.paragraph-2-part-1":
+            "I have had the opportunity to work on projects that allowed me to propose practical solutions",
+        "about-me.paragraph-2-part-2":
+            "It has been a rewarding experience that has allowed me to grow both technically and professionally",
+        "about-me.paragraph-3":
+            "I am a person committed to constant growth, I enjoy exploring new technologies and perfecting what I already know. I love sharing my knowledge with others, whether helping colleagues or participating in collaborative projects. I am always open to new ideas and approaches to improve and add value to everything I do.",
+        // Footer
+        "footer.paragraph-1": "Thanks for coming this far!",
+        "footer.paragraph-2": "This site was made inspired by the portfolio of",
+        "footer.paragraph-3": "and it works thanks to",
+        "footer.paragraph-4": "and",
+    },
+    es: {
+        //Header
+        "header.experience": "Experiencia",
+        "header.projects": "Proyectos",
+        "header.technologyStack": "Stack Tecnológico",
+        "header.education": "Educación",
+        "header.certificates": "Cursos y Certificados",
+        "header.aboutMe": "Sobre mí",
+        "header.contact": "Contacto",
+        // HomePage
+        "home-page.title": "Portfolio | Jehicoob",
+        "home-page.description": "Bienvenido a mi portafolio",
+        "home-page.experience": "Experiencia",
+        "home-page.projects": "Proyectos",
+        "home-page.technologyStack": "Stack Tecnológico",
+        "home-page.education": "Educación",
+        "home-page.certificates": "Cursos y Certificados",
+        "home-page.aboutMe": "Sobre mí",
+        // Hero
+        "hero.title": "Jehicoob López",
+        "hero.description.profession": "Desarrollador Full-Stack",
+        "hero.description.degree": "Tecnólogo en Electrónica",
+        "hero.description.part-1": "Más de 1 año de experiencia.",
+        "hero.description.part-2":
+            "de Cali, Colombia 🇨🇴, apasionado por transformar ideas en soluciones digitales efectivas y funcionales.",
+        // Experience
+        "experience.company-1-name": "Freelancer",
+        "experience.company-1-position": "Desarrollador Full-Stack",
+        "experience.company-1-start": "may. 2022",
+        "experience.company-1-end": "Presente",
+        "experience.company-1-description":
+            "Responsable del mantenimiento continuo y soporte técnico, asegurando la estabilidad y funcionalidad de aplicaciones web. Diseño y desarrollo de un sistema de chat integrado en sus sitios web, con mas de 150 usuarios activos por dia.",
+        "experience.company-2-name": "Avidesa Mac Pollo S.A.",
+        "experience.company-2-position":
+            "Técnico en Sistemas (Práctica Laboral)",
+        "experience.company-2-start": "oct. 2021",
+        "experience.company-2-end": "abr. 2022",
+        "experience.company-2-description":
+            "Brindé soporte técnico a usuarios internos, resolviendo incidencias relacionadas con hardware, software y redes. Realicé tareas básicas de mantenimiento de sistemas, configuración de equipos y documentación de procesos de TI, contribuyendo a la eficiencia operativa del departamento.",
+        // Projects
+        "projects.chat-title": "Chat Empresarial",
+        "projects.chat-description":
+            "Chat empresarial con envío de archivos, audios e imágenes. Más de 150 usuarios activos por día. Creada desde cero con Vue.js, Firebase y Tailwind CSS.",
+        "projects.system-title": "Sistema de Gestión Empresarial",
+        "projects.system-description":
+            "Plataforma integral para gestión y desarrollo de talentos en el ámbito profesional. Creada con Laravel, jQuery, JavaScript y CSS.",
+        // Technology Stack
+        "technology-stack.languages": "Lenguajes",
+        "technology-stack.fronted": "Frontend",
+        "technology-stack.backend": "Backend",
+        "technology-stack.database": "Base de Datos",
+        "technology-stack.tools": "Herramientas",
+        // Education
+        "education.education-1-name": "Universidad del Valle",
+        "education.education-1-degree": "Tecnólogo en Electrónica",
+        "education.education-1-start": "jun. 2017",
+        "education.education-1-end": "jul. 2022",
+        "education.education-1-description":
+            "Formación en sistemas electrónicos, circuitos digitales y análogos, diseño y mantenimiento de dispositivos electrónicos, aplicando conocimientos en contextos industriales y tecnológicos.",
+        "education.education-2-name":
+            "SENA - Centro Nacional de Asistencia Técnica a la industria (ASTIN)",
+        "education.education-2-degree": "Técnico en Sistemas",
+        "education.education-2-start": "ene. 2021",
+        "education.education-2-end": "abr. 2022",
+        "education.education-2-description":
+            "Enfoque en soporte técnico, mantenimiento de hardware y software, instalación de sistemas operativos y redes básicas, brindando soluciones eficientes a problemas informáticos.",
+        // About Me
+        "about-me.underlined-1": "estudiante autodidacta",
+        "about-me.underlined-2":
+            "desarrollando nuevas funcionalidades y mejorando las existentes en su plataforma web",
+        "about-me.paragraph-1-part-1":
+            "¡Hola! Me llamo Jehicoob, y mi camino en la programación comenzó con proyectos universitarios enfocados en Arduino. Desde que termine la universidad, he sido un",
+        "about-me.paragraph-1-part-2":
+            "explorando el mundo de la tecnología y el desarrollo de software",
+        "about-me.paragraph-2-part-1":
+            "He tenido la oportunidad de trabajar en proyectos que me permitieron proponer soluciones prácticas",
+        "about-me.paragraph-2-part-2":
+            "Ha sido una experiencia enriquecedora que me ha permitido crecer tanto técnica como profesionalmente",
+        "about-me.paragraph-3":
+            "Soy una persona comprometida con el crecimiento constante, disfruto explorar nuevas tecnologías y perfeccionar lo que ya sé. Me encanta compartir mis conocimientos con otros, ya sea ayudando a compañeros o participando en proyectos colaborativos, Siempre estoy abierto a nuevas ideas y enfoques para mejorar y aportar valor en todo lo que hago",
+        // Footer
+        "footer.paragraph-1": "¡Gracias por llegar hasta aquí!",
+        "footer.paragraph-2": "Este sitio se hizo inspirado en el porfolio de",
+        "footer.paragraph-3": "y funciona gracias a",
+        "footer.paragraph-4": "y",
+    },
+};
